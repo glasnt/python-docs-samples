1f8bcbb7c866def8eef3e43ac0a966c97c89339f
Katie McLaughlin <katie@glasnt.com>
2021-01-12 09:04:35 +1100

    WIP - SM + PSQL-ify sample

--- appengine/standard_python3/django/mysite/settings.py
+++ appengine/standard_python3/django/mysite/settings.py
@@ -11,17 +11,28 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import io
+import environ
+import google.auth
+from google.cloud import secretmanager_v1 as sm
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# [START appengine_django_secretconfig ]
+# Get Django settings from Secret Manager
+client = sm.SecretManagerServiceClient()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+_, project = google.auth.default()
+settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+name = f"projects/{project}/secrets/{settings_name}/versions/latest"
+payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# Update the secret key to a value of your own before deploying the app.
-SECRET_KEY = 'lldtg$9(wi49j_hpv8nnqlh!cj7kmbwq0$rj7vy(b(b30vlyzj'
+env = environ.Env()
+env.read_env(io.StringIO(payload))
+# [START appengine_django_secretconfig ]
+
+SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,45 +89,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+# [START appengine_django_dbconfig]
+# Use django-environ to define the connection string
+DATABASES = {"default": env.db()}
 
-# Install PyMySQL as mysqlclient/MySQLdb to use Django's mysqlclient adapter
-# See https://docs.djangoproject.com/en/2.1/ref/databases/#mysql-db-api-drivers
-# for more information
-import pymysql  # noqa: 402
-pymysql.version_info = (1, 4, 6, 'final', 0)  # change mysqlclient version
-pymysql.install_as_MySQLdb()
-
-# [START db_setup]
-if os.getenv('GAE_APPLICATION', None):
-    # Running on production App Engine, so connect to Google Cloud SQL using
-    # the unix socket at /cloudsql/<your-cloudsql-connection string>
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/[YOUR-CONNECTION-NAME]',
-            'USER': '[YOUR-USERNAME]',
-            'PASSWORD': '[YOUR-PASSWORD]',
-            'NAME': '[YOUR-DATABASE]',
-        }
-    }
-else:
-    # Running locally so connect to either a local MySQL instance or connect to
-    # Cloud SQL via the proxy. To start the proxy via command line:
-    #
-    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-    #
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-            'NAME': '[YOUR-DATABASE]',
-            'USER': '[YOUR-USERNAME]',
-            'PASSWORD': '[YOUR-PASSWORD]',
-        }
-    }
-# [END db_setup]
+# if running locally, presume Cloud SQL Proxy is running
+if not os.getenv('GAE_APPLICATION', None):
+    DATABASES["default"]["HOST"] = '127.0.0.1'
+    DATABASES["default"]["PORT"] = '5432'
+# [END appengine_django_dbconfig]
 
 # Use a in-memory sqlite3 database when testing in CI systems
 if os.getenv('TRAMPOLINE_CI', None):
